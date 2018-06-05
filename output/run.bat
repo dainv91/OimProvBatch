3fdab4b23c1c8745fa3ea95a6f1617a7f59f0466
Van Dai Nguyen <dainv91@gmail.com>
2018-06-05 10:47:32 +0700

    Update excel files + script

--- output/run.bat
+++ output/run.bat
@@ -1,6 +1,6 @@
-REM java -cp .;*;..\libs/*;..\libs\ooxml-lib/*;..\libs\poi-lib/*;..\libs\oim/*;..\libs\oim\common_lib/*;..\libs\oim\ext/*;..\libs\oim\lib/* vn.iadd.oim.user.test.UserBatch
-
 @echo off
+
+::java -cp .;*;..\libs/*;..\libs\ooxml-lib/*;..\libs\poi-lib/*;..\libs\oim/*;..\libs\oim\common_lib/*;..\libs\oim\ext/*;..\libs\oim\lib/* vn.iadd.oim.user.test.UserBatch
 
 setlocal
 call set_classpath.bat
--- output/set_classpath.bat
+++ output/set_classpath.bat
@@ -1,1 +1,3 @@
+REM Set class path
+
 set CLASSPATH=;*;..\libs/*;..\libs\ooxml-lib/*;..\libs\poi-lib/*;..\libs\oim/*;..\libs\oim\common_lib/*;..\libs\oim\ext/*;..\libs\oim\lib/*;
